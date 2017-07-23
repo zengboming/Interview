ef1a0787a118ead8529ad7fcc8506a919a65b4b2
zbm <520140341@qq.com>
2017-07-24 01:06:07 +0800

    AVLNode

--- Search/src/AVLTree.java
+++ Search/src/AVLTree.java
@@ -1,4 +1,4 @@
-import BinarySearchTree.Node;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class AVLTree {
 
@@ -22,7 +22,15 @@ public class AVLTree {
 		root = null;
 	}
 	
-	public int getHeight(AVLNode node) {
+	public int getHeight() {
+		if (root == null) {
+			return 0;
+		}
+		
+		return getHeight(root);
+	}
+	
+	private int getHeight(AVLNode node) {
 		if (node != null) {
 			return node.height; 
 		}
@@ -97,6 +105,101 @@ public class AVLTree {
 		node.height = max(getHeight(node.left), getHeight(node.right));
 		
 		return node;
+	}
+	
+	public AVLNode remove(int value) {
+		return remove(root, value);
+	}
+	
+	private AVLNode remove(AVLNode node, int value) {
+		if (node == null) {
+			return null;
+		}
+		
+		//删除结点在左子树
+		if (value < node.value) {
+			node.left = remove(node.left, value);
+			
+			if (getHeight(node.right) - getHeight(node.left) > 1) {
+				AVLNode rNode = node.right;
+				//RL
+				if (getHeight(rNode.left) > getHeight(rNode.right)) {
+					node = rightLeftRotation(node);
+				} else {   //RR
+					node = rightRightRotation(node);
+				}
+			}
+		} else if (value > node.value) {
+			node.right = remove(node.right, value);
+			
+			if (getHeight(node.left) - getHeight(node.right) > 1) {
+				AVLNode rNode = node.left;
+				//LR
+				if (getHeight(rNode.right) > getHeight(rNode.left)) {
+					node = leftRightRotation(node);
+				} else { //LL
+					node = leftLeftRotation(node);
+				}
+			}
+		} else {	//要删除的结点
+			if ((node.left != null) && (node.right != null)) {
+				//如果左子树更高，将左子树中最大的结点替换node，并删除最大结点
+				//采用这种方式的好处是：删除"tree的左子树中最大节点"之后，AVL树仍然是平衡的。
+				if (getHeight(node.left) > getHeight(node.right)) {
+					AVLNode max = getMax(node.left);
+					node.value = max.value;
+					node.left = remove(node.left, max.value);
+				} else {
+					//如果右子树高，将左子树中最小的结点替换node，并删除最小结点
+					//采用这种方式的好处是：删除"tree的右子树中最小节点"之后，AVL树仍然是平衡的。
+					AVLNode min = getMin(node.right);
+					node.value = min.value;
+					node.right = remove(node.right, min.value);
+				}
+			} else if (node.left != null) {
+				return node.left;
+			} else if (node.right != null) {
+				return node.right;
+			} else {
+				return null;
+			}
+		}
+		return node;
+	}
+	
+	public AVLNode getMax(AVLNode tree) {
+		if (tree == null) {
+			return null;
+		}
+		while(tree.right != null) {
+			tree = tree.right;
+		}
+		return tree;
+	}
+	
+	public AVLNode getMin(AVLNode tree) {
+		if (tree == null) {
+			return null;
+		}
+		while(tree.left != null) {
+			tree = tree.left;
+		}
+		return tree;
+	}
+	
+	public AVLNode get(int value) {		
+		AVLNode node = root;
+		while (node != null) {
+			if (value > node.value) {
+				node = node.right;
+			} else if (value < node.value) {
+				node = node.left;
+			} else {
+				return node;
+			}
+		}
+		
+		return null;
 	}
 	
 	
--- Search/src/Main.java
+++ Search/src/Main.java
@@ -4,29 +4,50 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int a[] = {3, 2, 1, 5, 6, 7, 9};
-		//int result ;
-		//Search search = new Search();
-		//result = search.binarySearch(a, 0, 6, 9);
-		//result = search.insertionSearch(a, 0, 6, 8);
-		BinarySearchTree tree = new BinarySearchTree();
-		for (int i = 0; i < a.length; i++) {
-			tree.put(a[i]);
-		}
-		BinarySearchTree.Node node = tree.get(5);
-		if (tree.get(5) != null) {
-			System.out.println(node.value);
-		}
+//		int a[] = {3, 2, 1, 5, 6, 7, 9};
+//		//int result ;
+//		//Search search = new Search();
+//		//result = search.binarySearch(a, 0, 6, 9);
+//		//result = search.insertionSearch(a, 0, 6, 8);
+//		BinarySearchTree tree = new BinarySearchTree();
+//		for (int i = 0; i < a.length; i++) {
+//			tree.put(a[i]);
+//		}
+//		BinarySearchTree.Node node = tree.get(5);
+//		if (tree.get(5) != null) {
+//			System.out.println(node.value);
+//		}
+//
+//		tree.getFontOrder();
+//		System.out.println();
+//		tree.getMiddleOrder();
+//		System.out.println();
+//		tree.getBehindOrder();
+//		System.out.println();
+		
+		
+		int arr[]= {3,2,1,4,5,6,7,16,15,14,13,12,11,10,8,9};
+		
+		int i;
+		AVLTree tree = new AVLTree();
+		
+		System.out.printf("== 依次添加: ");
+		for(i=0; i<arr.length; i++) {
+			System.out.printf("%d ", arr[i]);
+			tree.insert(arr[i]);
+		 }
+		
+		 
+		System.out.printf("== 高度: %d\n", tree.getHeight());
+		System.out.printf("== 树的详细信息: \n");
+		 
+		i = 8;
+		System.out.printf("\n== 删除根节点: %d", i);
+		tree.remove(i);
+		
+		System.out.printf("\n== 高度: %d", tree.getHeight());
 
-		tree.getFontOrder();
-		System.out.println();
-		tree.getMiddleOrder();
-		System.out.println();
-		tree.getBehindOrder();
-		System.out.println();
-		
-		
-		
+
 	}
 	
 }
