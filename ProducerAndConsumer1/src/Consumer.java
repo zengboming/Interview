cc40e439b6e11e37aaa6d0c08d6c3cb94d350cb8
zbm <520140341@qq.com>
2017-07-29 23:56:43 +0800

    1

---
+++ ProducerAndConsumer1/src/Consumer.java
@@ -0,0 +1,19 @@
+//Ïû·ÑÕß
+public class Consumer implements Runnable {
+	
+	private Storage storage;
+	private String name;
+	
+	public Consumer(Storage storage, String name) {
+		this.storage = storage;
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			storage.consume();
+		}
+		
+	} 
+}
---
+++ ProducerAndConsumer1/src/Producer.java
@@ -0,0 +1,18 @@
+//ЩњВњеп
+public class Producer implements Runnable {
+	
+	private Storage storage;
+	private String name;
+	
+	public Producer(Storage storage, String name) {
+		this.storage = storage;
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			storage.produce();
+		}
+	}
+}
